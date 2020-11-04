31eb73de93c882253f4f6396ccd1d4965f1b5df9
Ruger <ruben_27895@hotmail.com>
2020-11-04 18:02:33 +0100

    Practica 01

---
+++ app/scripts/P01/p01.js
@@ -0,0 +1,39 @@
+/* Variables declaration */
+const NUM_COMBINATIONS = 50;
+const LENGTH_COMBINATIONS = 6;
+const MIN_VAL = 1;
+const MAX_VAL = 49;
+// eslint-disable-next-line no-array-constructor
+const COMBINATIONS = new Array();
+
+for (
+  let numCombinationsMade = 0;
+  numCombinationsMade < NUM_COMBINATIONS;
+  numCombinationsMade++
+) {
+  const combination = new Set(); // Set avoid having duplicated values
+  let lengthActualCombination = 0; // Each combination start this to 0
+
+  while (lengthActualCombination < LENGTH_COMBINATIONS) {
+    const randomNumber =
+      Math.floor(Math.random() * (MAX_VAL - MIN_VAL)) + MIN_VAL; // Generate random number between 1 and 49 both included
+
+    if (!combination.has(randomNumber)) {
+      // If Set doesnt have the random number, we add it
+      combination.add(randomNumber);
+      lengthActualCombination++;
+    }
+  }
+
+  COMBINATIONS[numCombinationsMade] = combination;
+}
+
+for (const combination of COMBINATIONS) {
+  let message = 'Combinacion: ';
+
+  for (const number of combination) {
+    message += '[' + number + ']';
+  }
+
+  console.log(message);
+}
